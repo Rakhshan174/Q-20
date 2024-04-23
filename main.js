6ea27349a33c57ed5a8c165d3d9fe26d700b1ea4
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:03:46 -0700

    Q 20 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Think of something you could store in a array. For example, you Could make a list
+// of mountain, rivers, countries, cities, languages, or anything else you'd like.
+// write a program that creates a list containing these items.
+let country = ["Pkistan", "India", "NewYork", "Japan", "China"];
+console.log("List of country:");
+console.log(country);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Think of something you could store in a array. For example, you Could make a list
+// of mountain, rivers, countries, cities, languages, or anything else you'd like.
+// write a program that creates a list containing these items.
+
+let country: string[] =["Pkistan", "India", "NewYork", "Japan", "China"]
+console.log("List of country:");
+console.log(country);
